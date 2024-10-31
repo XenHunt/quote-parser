273fd45e97c2e55c8b7d254869f1667c688269fc
XenHunt <95288387+XenHunt@users.noreply.github.com>
2024-10-31 14:46:19 +0300

    Added README.md.

--- quoteparsing/quoteparsing/pipelines.py
+++ quoteparsing/quoteparsing/pipelines.py
@@ -14,7 +14,9 @@ from .items import QuoteparsingItem
 class QuoteparsingPipeline:
     def process_item(self, item: QuoteparsingItem, spider):  # default method
         # calling dumps to create json data.
-        line = json.dumps(dict(item), ensure_ascii=False) + ",\n"
+        line = "," if self.printcomma else ""
+        line = line + json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.printcomma = True
         # converting item to dict above, since dumps only intakes dict.
         self.file.write(line)  # writing content in output file.
         return item
@@ -22,6 +24,7 @@ class QuoteparsingPipeline:
     def open_spider(self, spider):
         self.file = open("result.json", "w", encoding="utf8")
         self.file.write("[\n")
+        self.printcomma = False
 
     def close_spider(self, spider):
         self.file.write("]\n")
